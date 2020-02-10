d35c3a849dff266a3a3d82d88229a7bdb01c25e8
Bartosz Michalak <bartosz.michalak@smart-citizen.pl>
2020-02-10 12:30:40 +0100

    Init repository. Add user model.

---
+++ config/test.exs
@@ -0,0 +1,19 @@
+use Mix.Config
+
+# Configure your database
+config :my_app, MyApp.Repo,
+  username:  System.get_env("POSTGRES_USERNAME"),
+  password:  System.get_env("POSTGRES_PASSWORD"),
+  database: "phoenix_tutorial_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :my_app, MyAppWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+config :bcrypt_elixir, :log_rounds, 4
